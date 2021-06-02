811a348de70a2c955f16d527e5fee9e58f3b085f
Gaetan Manchon <gmanchon@gmail.com>
2021-06-02 22:58:12 +0200

    add option to list machine types with filter

--- gce_machine_type/machine_type.py
+++ gce_machine_type/machine_type.py
@@ -5,13 +5,20 @@ import os
 import subprocess
 
 
-def fetch_machine_types():
+# store csv at package location
+data_file_path_fwf = os.path.join(
+    os.path.dirname(__file__),
+    "gce_machine_types.fwf"
+)
 
-    # store csv at package location
-    data_file_path_fwf = os.path.join(
-        os.path.dirname(__file__),
-        "gce_machine_types.fwf"
-    )
+# csv path
+data_file_path_csv = os.path.splitext(data_file_path_fwf)[0] + ".csv"
+
+
+def fetch_machine_types():
+    """
+    retrieve machine type from gcloud
+    """
 
     # open file for writing
     with open(data_file_path_fwf, "w") as file:
@@ -25,17 +32,43 @@ def fetch_machine_types():
             "--quiet"],
             stdout=file)
 
-    # csv path
-    data_file_path_csv = os.path.splitext(data_file_path_fwf)[0] + ".csv"
-
     # convert fixed width format to csv
     df = pd.read_fwf(data_file_path_fwf, widths=[18, 27, 6, 11, 100])
     df.to_csv(data_file_path_csv, index=False)
 
 
-def list_machine_types():
-    pass
+def list_machine_types(sort_by=None, sort_ascending=None, **kwargs):
+    """
+    display retrieved machine types
+    """
+
+    # load the data
+    df = pd.read_csv(data_file_path_csv)
+
+    # column params
+    column_params = dict(
+        zone="ZONE",
+        cpu="CPUS",
+        memory="MEMORY_GB")
+
+    # iterate through filters
+    for column, value in kwargs.items():
+
+        print(column, value, column_params[column])
+        df = df[df[column_params[column]] == value]
+
+    # show all rows
+    pd.set_option("display.max_rows", None)
+
+    print(df)
 
 
 if __name__ == '__main__':
-    fetch_machine_types()
+
+    # fetch_machine_types()
+
+    list_machine_types(
+        zone="europe-west1-b",
+        cpu=4,
+        sort_by=["memory", "cpu"],
+        sort_ascending=["desc", "asc"])
